bbaef8992083b99ffd45c65c40887acbff0e163e
unknown <efsannart@gmail.com>
2024-12-18 12:22:57 +0300

    .gitignore dosyası doğru konuma taşındı

---
+++ PatikaBootcampTask1/PatikaBootcampTask1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // Bu  ifade system ad alanını programa dahil eder, bu ad alanı temel .Net sınıflarını içerir
+
+namespace SelfIntroduction //namespace kodu düzenlemek ve isim çakışmalarını önlemek için kullanılan kapsayıcıdır.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Adımızı soyadımızı erkana yazdırıyoruz
+            Console.WriteLine("Efşan Nart");
+            //Bootcampde olma sebebimizi açıklıyoruz
+            Console.WriteLine("Yazılım alanında kendimi geliştirmek ve kariyerime yeni bir yön vermek için.");
+
+        }
+    }
+}
